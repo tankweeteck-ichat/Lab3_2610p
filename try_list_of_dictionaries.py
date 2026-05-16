5ca4a85723f741d17aa63ee83751ebc1e4c11f9a
KweeTeck <tan_kwee_teck@ichat.sp.edu.sg>
2026-05-16 10:20:13 +0100

    Added Feature 5 to employee_info.py and save as try_list_of_dictionaries.py

---
+++ try_list_of_dictionaries.py
@@ -0,0 +1,115 @@
+# Define a dictionary to store employee information
+employee_data = [
+    {"name": "John", "age": 30, "department": "Sales", "salary": 50000},
+    {"name": "Jane", "age": 25, "department": "Marketing", "salary": 60000},
+    {"name": "Mary", "age": 23, "department": "Marketing", "salary": 56000},
+    {"name": "Chloe",  "age": 35, "department": "Engineering", "salary": 70000},
+    {"name": "Mike", "age": 32, "department": "Engineering", "salary": 65000},
+    {"name": "Peter", "age": 40, "department": "Sales", "salary": 60000}
+]
+
+
+def show_employee_by_name(staffname):
+    for eachRecord in employee_data:
+        if (eachRecord["name"]==staffname):
+            print("Name = ", eachRecord["name"])
+            print("Age = ", eachRecord["age"])
+            print("Dept = ", eachRecord["department"])
+            print("Salary = ", eachRecord["salary"])
+            
+
+
+def get_employees_by_age_range(age_lower_limit, age_upper_limit):
+    result = []
+
+    # check for age limits and append the item to result
+    for item in employee_data:
+        if int(item["age"]) > int(age_lower_limit) and int(item["age"]) < int(age_upper_limit):
+            result.append(item)
+
+    return result
+
+def calculate_average_salary():
+    total = 0
+    average = 0
+
+    #add your implementation to calculate here
+    for eachRecord in employee_data:
+        total += eachRecord['salary']
+
+    average = total/len(employee_data)
+    return round(average, 2)
+
+def get_employees_by_dept(department):
+    result = []
+
+    # Add your implementation from here
+    for eachRecord in employee_data:
+        if eachRecord['department'] == department:
+            result.append(eachRecord)
+
+    return result
+
+def display_all_records():
+    print(("Name" + "\t" +"Age" +"\t" +"Department" +"\t" +"Salary" ).expandtabs(15))
+    for item in employee_data:
+        print((item["name"] + "\t" + str(item["age"]) + "\t" + item["department"] + "\t" + str(item["salary"])).expandtabs(15))
+
+
+def display_records(employee_info):
+    print(("Name" + "\t" +"Age" +"\t" +"Department" +"\t" +"Salary" ).expandtabs(15))
+    for item in employee_info:
+        print((item["name"] + "\t" + str(item["age"]) + "\t" + item["department"] + "\t" + str(item["salary"])).expandtabs(15))
+
+def display_main_menu():
+
+    print("\n----- Employee information Tracker -----")
+
+    print("Select option\n")
+
+    print("1 - Display all records")
+    print("2 - Display average salary")
+    print("3 - Display employee within age range")
+    print("4 - Display employee in a department")
+    print("5 - Display employee info")
+
+    print("Q - Quit")
+
+    option = input("Enter selection =>")
+
+    if option == '1':
+        display_all_records()
+
+    elif option == '2':
+        average_salary = calculate_average_salary()
+        print("Average salary = " + str(average_salary))
+
+    elif option == '3':
+        age_lower_limit = input("age (Lower Limit) = ")
+        age_upper_limit = input("age (Uper Limit) = ")
+        employee_info = get_employees_by_age_range(age_lower_limit, age_upper_limit)
+        display_records(employee_info)
+
+
+    elif option == '4':
+        department = input("Name of Department = ")
+        employee_info = get_employees_by_dept(department)
+        display_records(employee_info)
+
+
+    elif option == '5':
+        staffname = input("Name of employee = ")
+        show_employee_by_name(staffname)
+
+
+    elif option == 'Q':
+        quit()
+
+def main():
+
+    while (True):
+        display_main_menu()
+
+
+if __name__ == "__main__":
+    main()
